628f71ea69b40d03edc4ede071936b2d30f56a4a
Cavin Lee <lihao921@gmail.com>
2014-11-19 10:54:05 +0800

    add Note project

---
+++ Note/src/com/database/test/DatabaseHelper.java
@@ -0,0 +1,24 @@
+package com.database.test;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+	public DatabaseHelper(Context context, String name, CursorFactory factory, int version) {
+		super(context, name, factory, version);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL("create table user (id int , name varchar(20))");
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+	}
+	
+}
